ce8b9b7aed762385b33822b7987d390991690266
DESKTOP-S6KAUUS\Acer <ppm421999@gmail.com>
2020-08-10 01:21:43 +0630

    Login setup

--- Firebase/app/src/main/java/com/example/firebase/MainActivity.kt
+++ Firebase/app/src/main/java/com/example/firebase/MainActivity.kt
@@ -1,25 +1,104 @@
 package com.example.firebase
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private const val RC_SIGN_IN  : Int = 123
+    }
+    private val btnSignIn by lazy {
+        findViewById<Button>(R.id.btnGoogleSignIn)
+    }
 
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var mAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        createRequest()
+        btnSignIn.setOnClickListener {
+            signIn()
+        }
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
+
+    private fun createRequest() {
+        val googleSignInOption = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+      mGoogleSignInClient = GoogleSignIn.getClient(this,googleSignInOption)
+    }
+    private fun signIn(){
+        val intent  = mGoogleSignInClient.signInIntent
+        startActivityForResult(intent,RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode== RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.result
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+            catch (e: ApiException){
+                Toast.makeText(this,"Google Sign In failed",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+         mAuth.signInWithCredential(credential)
+             .addOnCompleteListener(this){task ->
+                 if(task.isSuccessful){
+                     val user = mAuth.currentUser
+                     updateUI()
+                 }
+             }
+    }
+    override fun onStart() {
+        super.onStart()
+        val user = mAuth.currentUser
+        if(user!=null) {
+           updateUI()
+            finish()
+        }
+    }
+
+    fun updateUI(){
+        val intent =  Intent(applicationContext,LoginInSuccessActivity::class.java)
+        startActivity(intent)
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
